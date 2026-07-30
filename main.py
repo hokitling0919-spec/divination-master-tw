88441a7b36daf8070a0e0af833704279ab39ef14
hokitling0919-spec <hokitling0919@gmail.com>
2026-07-30 22:16:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from modules.draw_lot import run_draw_lot
 from modules.record_manager import read_all_record
+from modules.yijing import run_yijing #新增
 
 def main_menu():
     while True:
-        print("\n========= Divination Master TW 繁體占卜系統 =========")
-        print("1｜抽靈籤")
-        print("2｜查詢歷史抽籤紀錄")
-        print("3｜易經金錢起卦
-        print("0｜離開程式")
+        print("\n======= Divination Master TW 繁體占卜系統 =======")
+        print("(1 | 抽靈籤)")
+        print("(2 | 查詢歷史抽籤紀錄)")
+        print("(3 | 易經金錢起卦)")
+        print("(0 | 離開程式)")
         select = input("請輸入選項數字：")
         if select == "1":
             run_draw_lot()
         elif select == "2":
-            print("\n===== 歷史抽籤紀錄 =====")
+            print("\n====")
             data = read_all_record()
             print(data if data else "暫無任何抽籤紀錄")
+        elif select == "3":
+            run_yijing()  #易經起卦
         elif select == "0":
-            print("🙏 平安吉祥，程式結束")
+            print("🙏平安吉祥，程式結束")
             break
         else:
             print("❌ 輸入錯誤，請重新選擇！")
-
-if __name__ == "__main__":
-    main_menu()
